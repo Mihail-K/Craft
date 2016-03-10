1d796f116f2d2b857f4b8ac9cb58b41cb04a0c05
Mihail-K <Mihail.K@fimu.tk>
2016-03-09 22:28:39 -0500

    Use integer types.

--- source/craft/unittests/integer.d
+++ source/craft/unittests/integer.d
@@ -3,40 +3,43 @@ module craft.unittests.integer;
 
 import craft;
 
+import std.algorithm;
+import std.math;
+
 unittest // operators
 {
-    assert(`1 + 2`.run.coerceString ==  "3");
-    assert(`2 + 1`.run.coerceString ==  "3");
-    assert(`2 - 1`.run.coerceString ==  "1");
-    assert(`1 - 2`.run.coerceString == "-1");
+    assert(`1 + 2`.run.asLong ==  3);
+    assert(`2 + 1`.run.asLong ==  3);
+    assert(`2 - 1`.run.asLong ==  1);
+    assert(`1 - 2`.run.asLong == -1);
 
-    assert(`1 + 2 + 3`.run.coerceString == "6");
-    assert(`3 + 1 - 2`.run.coerceString == "2");
+    assert(`1 + 2 + 3`.run.asLong == 6);
+    assert(`3 + 1 - 2`.run.asLong == 2);
 
-    assert(`3 +  2 * 4 `.run.coerceString == "11");
-    assert(`3 *  2 + 4 `.run.coerceString == "10");
-    assert(`3 * (2 + 4)`.run.coerceString == "18");
+    assert(`3 +  2 * 4 `.run.asLong == 11);
+    assert(`3 *  2 + 4 `.run.asLong == 10);
+    assert(`3 * (2 + 4)`.run.asLong == 18);
 
-    assert(`+3 + -3`.run.coerceString ==  "0");
-    assert(`+3 - +3`.run.coerceString ==  "0");
-    assert(`-3 + -3`.run.coerceString == "-6");
-    assert(`-3 - -3`.run.coerceString ==  "0");
+    assert(`+3 + -3`.run.asLong ==  0);
+    assert(`+3 - +3`.run.asLong ==  0);
+    assert(`-3 + -3`.run.asLong == -6);
+    assert(`-3 - -3`.run.asLong ==  0);
 
-    assert(`1024 / 16`.run.coerceString ==    "64");
-    assert(`1024 * 16`.run.coerceString == "16384");
+    assert(`1024 / 16`.run.asLong ==    64);
+    assert(`1024 * 16`.run.asLong == 16384);
 
-    assert(`2730 | 324`.run.coerceString == "3054");
-    assert(` 740 &  82`.run.coerceString ==   "64");
-    assert(`4123 ^ 819`.run.coerceString == "4904");
+    assert(`2730 | 324`.run.asLong == 3054);
+    assert(` 740 &  82`.run.asLong ==   64);
+    assert(`4123 ^ 819`.run.asLong == 4904);
 }
 
 unittest // functions
 {
-    assert(`(-123).abs`.run.coerceString == "123");
-    assert(`(+123).abs`.run.coerceString == "123");
+    assert(`(-123).abs`.run.asLong == abs(-123));
+    assert(`(+123).abs`.run.asLong == abs(+123));
 
-    assert(`123.max(456)`.run.coerceString == "456");
-    assert(`123.min(456)`.run.coerceString == "123");
-    assert(`456.max(123)`.run.coerceString == "456");
-    assert(`456.min(123)`.run.coerceString == "123");
+    assert(`123.max(456)`.run.asLong == 123.max(456));
+    assert(`123.min(456)`.run.asLong == 123.min(456));
+    assert(`456.max(123)`.run.asLong == 456.max(123));
+    assert(`456.min(123)`.run.asLong == 123.min(123));
 }
